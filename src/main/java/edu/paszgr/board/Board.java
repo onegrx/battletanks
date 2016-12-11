feb98957241a8a674f7d790ec66bed6212c246ac
onegrx <onegrx@gmail.com>
2016-12-11 21:29:30 +0100

    Implement Board methods

--- src/main/java/edu/paszgr/board/Board.java
+++ src/main/java/edu/paszgr/board/Board.java
@@ -2,23 +2,23 @@ package edu.paszgr.board;
 
 import edu.paszgr.algo.Direction;
 import edu.paszgr.control.Tank;
-
 import java.util.*;
 import java.util.stream.Collectors;
+import static java.lang.Math.abs;
 
 public class Board {
-    private BoardSize boardSize = null;
-    private List<Tank> tanks = null;
+    private final BoardSize boardSize;
+    private List<Tank> tanks;
 
     public Board(BoardSize boardSize, List<Tank> tanks) {
         this.boardSize = boardSize;
         this.tanks = tanks;
     }
 
-
     public List<Tank> getTanksOnPosition(Position position) {
-        // TODO
-        return null;
+        return this.tanks.stream()
+                .filter(tank -> tank.getPosition() == position)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -26,18 +26,56 @@ public class Board {
      *          from given position (inclusively)
      *          and along the direction until the end of board
      * */
-    public List<Tank> getTanksOnTargetLine(Position position, Direction direction) {
-        // TODO
-        return null;
+    public List<Tank> getTanksOnTargetLine(Position p, Direction direction) {
+        switch (direction) {
+            case UP:
+                return tanks.stream()
+                        .filter(t -> t.getPosition().getX() > p.getX() && t.getPosition().getY() == p.getY())
+                        .collect(Collectors.toList());
+            case DOWN:
+                return tanks.stream()
+                        .filter(t -> t.getPosition().getX() < p.getX() && t.getPosition().getY() == p.getY())
+                        .collect(Collectors.toList());
+            case LEFT:
+                return tanks.stream()
+                        .filter(t -> t.getPosition().getY() < p.getY() && t.getPosition().getX() == p.getX())
+                        .collect(Collectors.toList());
+            case RIGHT:
+                return tanks.stream()
+                        .filter(t -> t.getPosition().getY() > p.getY() && t.getPosition().getX() == p.getX())
+                        .collect(Collectors.toList());
+            case UP_LEFT:
+                return tanks.stream()
+                        .filter(t -> t.getPosition().getX() > p.getX() && t.getPosition().getY() < p.getY())
+                        .filter(t -> abs(t.getPosition().getX() - p.getX()) == abs(t.getPosition().getY() - p.getY()))
+                        .collect(Collectors.toList());
+            case UP_RIGHT:
+                return tanks.stream()
+                        .filter(t -> t.getPosition().getX() > p.getX() && t.getPosition().getY() > p.getY())
+                        .filter(t -> abs(t.getPosition().getX() - p.getX()) == abs(t.getPosition().getY() - p.getY()))
+                        .collect(Collectors.toList());
+            case DOWN_LEFT:
+                return tanks.stream()
+                        .filter(t -> t.getPosition().getX() < p.getX() && t.getPosition().getY() < p.getY())
+                        .filter(t -> abs(t.getPosition().getX() - p.getX()) == abs(t.getPosition().getY() - p.getY()))
+                        .collect(Collectors.toList());
+            case DOWN_RIGHT:
+                return tanks.stream()
+                        .filter(t -> t.getPosition().getX() < p.getX() && t.getPosition().getY() > p.getY())
+                        .filter(t -> abs(t.getPosition().getX() - p.getX()) == abs(t.getPosition().getY() - p.getY()))
+                        .collect(Collectors.toList());
+            default:
+                return Collections.emptyList();
+        }
     }
+
 
     public Position getPositionOfTank(Tank tank) {
         return tank.getPosition();
     }
 
     public List<Tank> getAllTanks() {
-        // TODO
-        return null;
+        return tanks;
     }
 
     public BoardSize getBoardSize() {
