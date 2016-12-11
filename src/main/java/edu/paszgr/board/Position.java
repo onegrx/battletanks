063af51618ed70d6c996da85e7e493a5964b631b
onegrx <onegrx@gmail.com>
2016-12-11 21:53:37 +0100

    Position equals and hashCode

--- src/main/java/edu/paszgr/board/Position.java
+++ src/main/java/edu/paszgr/board/Position.java
@@ -16,4 +16,22 @@ public class Position {
     public int getX() {
         return x;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Position position = (Position) o;
+
+        return x == position.x && y == position.y;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
 }
