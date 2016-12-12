1fd547fdac608fc28bc24eba26d2e2c11cfdc88e
onegrx <onegrx@gmail.com>
2016-12-13 00:55:20 +0100

    Fix my test <3

--- src/test/java/edu/paszgr/board/ExecutionManagerTest.java
+++ src/test/java/edu/paszgr/board/ExecutionManagerTest.java
@@ -5,15 +5,14 @@ import edu.paszgr.algo.actions.Movement;
 import edu.paszgr.algo.algorithms.OnlyMoveAlgorithm;
 import edu.paszgr.control.Player;
 import edu.paszgr.control.Tank;
-import junit.framework.TestCase;
-import org.testng.annotations.Test;
-
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 
 /**
  * Created by onegrx on 11.12.16.
  */
-public class ExecutionManagerTest extends TestCase {
+public class ExecutionManagerTest {
 
     ExecutionManager executionManager = new ExecutionManager();
     Board board = mock(Board.class);
@@ -26,6 +25,7 @@ public class ExecutionManagerTest extends TestCase {
         Movement movement = new Movement(Direction.UP);
         Position position = new Position(1, 1);
         Tank tank = new Tank(player, board, 0, position);
+        player.createRoundStatistics(1);
 
         //when
         executionManager.executeTankAction(movement, tank, board);
@@ -43,6 +43,8 @@ public class ExecutionManagerTest extends TestCase {
         Movement movement = new Movement(Direction.LEFT);
         Position position = new Position(5, 3);
         Tank tank = new Tank(player, board, 0, position);
+        player.createRoundStatistics(1);
+
 
         //when
         executionManager.executeTankAction(movement, tank, board);
