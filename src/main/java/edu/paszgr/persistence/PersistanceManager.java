24e2673df78b791278973cfb4148a3a472c54e7b
onegrx <onegrx@gmail.com>
2017-01-01 20:59:56 +0100

    Serialize fields

--- src/main/java/edu/paszgr/persistence/PersistanceManager.java
+++ src/main/java/edu/paszgr/persistence/PersistanceManager.java
@@ -2,9 +2,11 @@ package edu.paszgr.persistence;
 
 import edu.paszgr.board.Field;
 
+import java.io.*;
 import java.util.List;
 
 public class PersistanceManager {
+
     public void saveGameStateToFile(GameState state, String fileName, boolean append) {
         // TODO
     }
@@ -15,11 +17,30 @@ public class PersistanceManager {
     }
 
     public void saveFieldsToFile(Field[][] fields, String fileName, boolean append) {
-        // TODO
+
+        try {
+            FileOutputStream fos = new FileOutputStream(fileName);
+            ObjectOutputStream outputStream = new ObjectOutputStream(fos);
+            outputStream.writeObject(fields);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public Field[][] readFieldsFromFile(String fileName) {
-        // TODO
-        return null;
+
+        Field[][] fields = null;
+
+        try {
+            FileInputStream fis = new FileInputStream(fileName);
+            ObjectInputStream iis = new ObjectInputStream(fis);
+            fields = (Field[][]) iis.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return fields;
+
     }
+
 }
