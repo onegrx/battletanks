a2224df6141f46cabd7f5466192d1e80ac765107
onegrx <onegrx@gmail.com>
2017-01-01 20:31:01 +0100

    Add TankDescriptor

--- src/main/java/edu/paszgr/persistence/GameState.java
+++ src/main/java/edu/paszgr/persistence/GameState.java
@@ -1,25 +1,15 @@
 package edu.paszgr.persistence;
 
-import edu.paszgr.board.Board;
-import edu.paszgr.control.Tank;
-
 import java.io.Serializable;
+import java.util.List;
 
 public class GameState implements Serializable {
-    private final int roundNumber;
-    private final int turnNumber;
-    private final int tankTurnNumber;
-    private final Tank latestTank;
-    private final Board board;
 
-
-    public GameState(int roundNumber, int turnNumber, int tankTurnNumber, Tank latestTank, Board board) {
-        this.roundNumber = roundNumber;
-        this.turnNumber = turnNumber;
-        this.tankTurnNumber = tankTurnNumber;
-        this.latestTank = latestTank;
-        this.board = board;
-    }
+    private int roundNumber;
+    private int turnNumber;
+    private int tankTurnNumber;
+    private TankDescriptor currentTant;
+    private List<TankDescriptor> allTanks;
 
     public int getRoundNumber() {
         return roundNumber;
@@ -29,15 +19,15 @@ public class GameState implements Serializable {
         return turnNumber;
     }
 
-    public Tank getLatestTank() {
-        return latestTank;
-    }
-
-    public Board getBoard() {
-        return board;
-    }
-
     public int getTankTurnNumber() {
         return tankTurnNumber;
+    }
+
+    public TankDescriptor getCurrentTant() {
+        return currentTant;
+    }
+
+    public List<TankDescriptor> getAllTanks() {
+        return allTanks;
     }
 }
